42bf6d324413cdd2c26b452d9c940801ea73f448
Ritwik Lodhiya <ritwikvl@gmail.com>
2024-03-30 00:01:35 -0700

    Removing some template content and replacing with my chosen styling, custom logo, and filler-text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,9 +24,15 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <div>
+            {`Built using `}
+            <Link href="https://nextjs.org/">Next.js</Link>
+            {` and `}
+            <Link href="https://tailwindcss.com/">Tailwind CSS</Link>
+            {` (and this `}
+            <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">template</Link>
+            {`)`}
+          </div>
         </div>
       </div>
     </footer>
