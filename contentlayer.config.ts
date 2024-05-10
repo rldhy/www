254ec16d5d6e74cfc1f864b4289e4002007436cc
Ritwik Lodhiya <ritwikvl@gmail.com>
2024-05-10 00:19:14 -0700

    Add Location to Author Layout

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -117,6 +117,7 @@ export const Authors = defineDocumentType(() => ({
   fields: {
     name: { type: 'string', required: true },
     avatar: { type: 'string' },
+    location: { type: 'string' },
     occupation: { type: 'string' },
     company: { type: 'string' },
     email: { type: 'string' },
