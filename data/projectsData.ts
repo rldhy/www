fb92293db69c3e95f50b967ee7ed05e5447f070b
Ritwik Lodhiya <ritwikvl@gmail.com>
2024-05-14 17:32:51 -0700

    Remove Template Content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,15 +5,6 @@ interface Project {
   imgSrc?: string
 }
 
-const projectsData: Project[] = [
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
+const projectsData: Project[] = []
 
 export default projectsData
