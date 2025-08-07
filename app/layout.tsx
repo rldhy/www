1252e07dcc0b4bf0774eb910403e9187920fc6b4
Ritwik Lodhiya <ritwikvl@gmail.com>
2025-08-07 10:52:02 -0700

    Update light BG color and remove links for x and mastadon

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="bg-gray-100 text-black antialiased transition-colors duration-200 dark:bg-gray-900 dark:text-white">
+      <body className="bg-gray-200 text-black antialiased transition-colors duration-200 dark:bg-gray-900 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
