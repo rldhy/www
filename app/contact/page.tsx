15534b4dd1cbd6078ef7aa1ebd83bbba8ca99a43
Ritwik Lodhiya <ritwikvl@gmail.com>
2026-02-12 11:50:36 -0800

    styling updates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,15 +6,18 @@ export const metadata = genPageMetadata({ title: 'Contact' })
 export default function Contact() {
   return (
     <>
-      <div className="divide-y-2">
+      <div className="divide-y-2 divide-gray-400 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Contact Me
           </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Please fill out the form below to reach out to me. Looking forward to hearing from you!
+          </p>
         </div>
-      </div>
-      <div className="min-w-full items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-        <GetInTouch />
+        <div className="container items-start justify-start">
+          <GetInTouch />
+        </div>
       </div>
     </>
   )
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,7 @@ export const metadata = genPageMetadata({ title: 'Projects' })
 export default function Projects() {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-400 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Projects
