3c4c06c696a25cc2857476fab85cccd9c7354404
Ritwik Lodhiya <ritwikvl@gmail.com>
2026-02-21 12:48:48 -0800

    Update packages and update Brevo API to use v4

--- services/email/brevo/brevo-service.ts
+++ services/email/brevo/brevo-service.ts
@@ -1,41 +1,34 @@
 import {
-  SendSmtpEmail,
-  TransactionalEmailsApi,
-  TransactionalEmailsApiApiKeys,
+  BrevoClient
 } from '@getbrevo/brevo'
 import { buildGetInTouchHtml, GetInTouchArgs } from '../message-builder'
 import { EmailProvider } from '../email-service'
 
 class BrevoService implements EmailProvider {
   private readonly apiKey: string
+  private readonly client: BrevoClient
 
   constructor() {
     if (!process.env.BREVO_API_KEY)
       throw new Error('BREVO_API_KEY is not defined in the environment variables')
     this.apiKey = process.env.BREVO_API_KEY
+    this.client = new BrevoClient({
+      apiKey: this.apiKey,
+    })
   }
 
   async sendGetInTouchMessage(args: GetInTouchArgs): Promise<boolean> {
     try {
       const htmlMsg = buildGetInTouchHtml(args)
 
-      const apiInstance = new TransactionalEmailsApi()
-      apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, this.apiKey)
+      const response = await this.client.transactionalEmails.sendTransacEmail({
+        subject: 'RLODHIYA.DEV GetInTouch Form Submission',
+        htmlContent: htmlMsg,
+        sender: { name: 'RL-System', email: 'system@rlodhiya.dev' },
+        to: [{ name: 'Ritwik Lodhiya', email: 'hello@rlodhiya.dev' }],
+      })
 
-      const message = new SendSmtpEmail()
-      message.subject = `RLODHIYA.DEV GetInTouch Form Submission`
-      message.htmlContent = htmlMsg
-      message.sender = { name: 'RL-System', email: 'system@rlodhiya.dev' }
-      message.to = [{ name: 'Ritwik Lodhiya', email: 'hello@rlodhiya.dev' }]
-
-      const { response } = await apiInstance.sendTransacEmail(message)
-      const { statusCode, statusMessage } = response
-      if (statusCode && statusCode >= 200 && statusCode < 300) {
-        console.log(`Brevo API call succeeded: status: ${statusCode}, message: ${statusMessage}`)
-      } else {
-        console.log(`Brevo API call failed: status: ${statusCode}, message: ${statusMessage}`)
-        return false
-      }
+      console.log(`Email Sent: ${response}`)
     } catch (error) {
       console.log(`Error sending email: ${error}`)
       return false
